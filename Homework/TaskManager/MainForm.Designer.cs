f717d8a9792d729f7c497b69ad34041f975d9406
alkeity <alkeity.23@gmail.com>
2024-11-30 15:22:12 +0300

    init

---
+++ Homework/TaskManager/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TaskManager
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPageProcesses = new System.Windows.Forms.TabPage();
+			this.listViewProcesses = new System.Windows.Forms.ListView();
+			this.btnEndTask = new System.Windows.Forms.Button();
+			this.tabControl.SuspendLayout();
+			this.tabPageProcesses.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPageProcesses);
+			this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl.Location = new System.Drawing.Point(0, 0);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(540, 350);
+			this.tabControl.TabIndex = 0;
+			// 
+			// tabPageProcesses
+			// 
+			this.tabPageProcesses.Controls.Add(this.btnEndTask);
+			this.tabPageProcesses.Controls.Add(this.listViewProcesses);
+			this.tabPageProcesses.Location = new System.Drawing.Point(4, 22);
+			this.tabPageProcesses.Name = "tabPageProcesses";
+			this.tabPageProcesses.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageProcesses.Size = new System.Drawing.Size(532, 324);
+			this.tabPageProcesses.TabIndex = 0;
+			this.tabPageProcesses.Text = "Processes";
+			this.tabPageProcesses.UseVisualStyleBackColor = true;
+			// 
+			// listViewProcesses
+			// 
+			this.listViewProcesses.HideSelection = false;
+			this.listViewProcesses.Location = new System.Drawing.Point(0, 0);
+			this.listViewProcesses.Name = "listViewProcesses";
+			this.listViewProcesses.Size = new System.Drawing.Size(532, 282);
+			this.listViewProcesses.TabIndex = 0;
+			this.listViewProcesses.UseCompatibleStateImageBehavior = false;
+			// 
+			// btnEndTask
+			// 
+			this.btnEndTask.Location = new System.Drawing.Point(449, 291);
+			this.btnEndTask.Name = "btnEndTask";
+			this.btnEndTask.Size = new System.Drawing.Size(75, 23);
+			this.btnEndTask.TabIndex = 1;
+			this.btnEndTask.Text = "End Task";
+			this.btnEndTask.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(540, 350);
+			this.Controls.Add(this.tabControl);
+			this.Name = "MainForm";
+			this.Text = "Task Manager";
+			this.tabControl.ResumeLayout(false);
+			this.tabPageProcesses.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPageProcesses;
+		private System.Windows.Forms.Button btnEndTask;
+		private System.Windows.Forms.ListView listViewProcesses;
+	}
+}
+
