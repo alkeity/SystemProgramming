d918f8c3b442b0077005c738fff06bfe4496fded
alkeity <alkeity.23@gmail.com>
2024-11-30 16:20:54 +0300

    feat: interface wip

--- Homework/TaskManager/MainForm.Designer.cs
+++ Homework/TaskManager/MainForm.Designer.cs
@@ -28,65 +28,171 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.tabControl = new System.Windows.Forms.TabControl();
 			this.tabPageProcesses = new System.Windows.Forms.TabPage();
-			this.listViewProcesses = new System.Windows.Forms.ListView();
 			this.btnEndTask = new System.Windows.Forms.Button();
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.startNewProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.topmostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hideWhenMinimizedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.updateNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.dataGridViewProcesses = new System.Windows.Forms.DataGridView();
 			this.tabControl.SuspendLayout();
 			this.tabPageProcesses.SuspendLayout();
+			this.menuStrip.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tabControl
 			// 
 			this.tabControl.Controls.Add(this.tabPageProcesses);
 			this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabControl.Location = new System.Drawing.Point(0, 0);
+			this.tabControl.Location = new System.Drawing.Point(0, 24);
 			this.tabControl.Name = "tabControl";
 			this.tabControl.SelectedIndex = 0;
-			this.tabControl.Size = new System.Drawing.Size(540, 350);
+			this.tabControl.Size = new System.Drawing.Size(540, 357);
 			this.tabControl.TabIndex = 0;
 			// 
 			// tabPageProcesses
 			// 
+			this.tabPageProcesses.Controls.Add(this.dataGridViewProcesses);
 			this.tabPageProcesses.Controls.Add(this.btnEndTask);
-			this.tabPageProcesses.Controls.Add(this.listViewProcesses);
 			this.tabPageProcesses.Location = new System.Drawing.Point(4, 22);
 			this.tabPageProcesses.Name = "tabPageProcesses";
 			this.tabPageProcesses.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageProcesses.Size = new System.Drawing.Size(532, 324);
+			this.tabPageProcesses.Size = new System.Drawing.Size(532, 331);
 			this.tabPageProcesses.TabIndex = 0;
 			this.tabPageProcesses.Text = "Processes";
 			this.tabPageProcesses.UseVisualStyleBackColor = true;
 			// 
-			// listViewProcesses
-			// 
-			this.listViewProcesses.HideSelection = false;
-			this.listViewProcesses.Location = new System.Drawing.Point(0, 0);
-			this.listViewProcesses.Name = "listViewProcesses";
-			this.listViewProcesses.Size = new System.Drawing.Size(532, 282);
-			this.listViewProcesses.TabIndex = 0;
-			this.listViewProcesses.UseCompatibleStateImageBehavior = false;
-			// 
 			// btnEndTask
 			// 
-			this.btnEndTask.Location = new System.Drawing.Point(449, 291);
+			this.btnEndTask.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnEndTask.Location = new System.Drawing.Point(440, 298);
 			this.btnEndTask.Name = "btnEndTask";
-			this.btnEndTask.Size = new System.Drawing.Size(75, 23);
+			this.btnEndTask.Size = new System.Drawing.Size(83, 23);
 			this.btnEndTask.TabIndex = 1;
 			this.btnEndTask.Text = "End Task";
 			this.btnEndTask.UseVisualStyleBackColor = true;
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.viewToolStripMenuItem});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(540, 24);
+			this.menuStrip.TabIndex = 1;
+			this.menuStrip.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startNewProcessToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// startNewProcessToolStripMenuItem
+			// 
+			this.startNewProcessToolStripMenuItem.Name = "startNewProcessToolStripMenuItem";
+			this.startNewProcessToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+			this.startNewProcessToolStripMenuItem.Text = "Start new process";
+			this.startNewProcessToolStripMenuItem.Click += new System.EventHandler(this.startNewProcessToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// optionsToolStripMenuItem
+			// 
+			this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topmostToolStripMenuItem,
+            this.hideWhenMinimizedToolStripMenuItem});
+			this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+			this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+			this.optionsToolStripMenuItem.Text = "Options";
+			// 
+			// topmostToolStripMenuItem
+			// 
+			this.topmostToolStripMenuItem.Name = "topmostToolStripMenuItem";
+			this.topmostToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+			this.topmostToolStripMenuItem.Text = "Always on top";
+			this.topmostToolStripMenuItem.Click += new System.EventHandler(this.topmostToolStripMenuItem_Click);
+			// 
+			// hideWhenMinimizedToolStripMenuItem
+			// 
+			this.hideWhenMinimizedToolStripMenuItem.Name = "hideWhenMinimizedToolStripMenuItem";
+			this.hideWhenMinimizedToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+			this.hideWhenMinimizedToolStripMenuItem.Text = "Hide when minimized";
+			this.hideWhenMinimizedToolStripMenuItem.Click += new System.EventHandler(this.hideWhenMinimizedToolStripMenuItem_Click);
+			// 
+			// viewToolStripMenuItem
+			// 
+			this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateNowToolStripMenuItem});
+			this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+			this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.viewToolStripMenuItem.Text = "View";
+			// 
+			// updateNowToolStripMenuItem
+			// 
+			this.updateNowToolStripMenuItem.Name = "updateNowToolStripMenuItem";
+			this.updateNowToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+			this.updateNowToolStripMenuItem.Text = "Update now";
+			this.updateNowToolStripMenuItem.Click += new System.EventHandler(this.updateNowToolStripMenuItem_Click);
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.Text = "Task Manager";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+			// 
+			// dataGridViewProcesses
+			// 
+			this.dataGridViewProcesses.AllowUserToOrderColumns = true;
+			this.dataGridViewProcesses.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridViewProcesses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewProcesses.Location = new System.Drawing.Point(0, 0);
+			this.dataGridViewProcesses.Name = "dataGridViewProcesses";
+			this.dataGridViewProcesses.Size = new System.Drawing.Size(532, 289);
+			this.dataGridViewProcesses.TabIndex = 2;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(540, 350);
+			this.ClientSize = new System.Drawing.Size(540, 381);
 			this.Controls.Add(this.tabControl);
+			this.Controls.Add(this.menuStrip);
+			this.MainMenuStrip = this.menuStrip;
+			this.MinimumSize = new System.Drawing.Size(556, 420);
 			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Task Manager";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.Resize += new System.EventHandler(this.MainForm_Resize);
 			this.tabControl.ResumeLayout(false);
 			this.tabPageProcesses.ResumeLayout(false);
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcesses)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -95,7 +201,17 @@
 		private System.Windows.Forms.TabControl tabControl;
 		private System.Windows.Forms.TabPage tabPageProcesses;
 		private System.Windows.Forms.Button btnEndTask;
-		private System.Windows.Forms.ListView listViewProcesses;
+		private System.Windows.Forms.MenuStrip menuStrip;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem startNewProcessToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem topmostToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem updateNowToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hideWhenMinimizedToolStripMenuItem;
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.DataGridView dataGridViewProcesses;
 	}
 }
 
--- Homework/TaskManager/MainForm.cs
+++ Homework/TaskManager/MainForm.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,62 @@ namespace TaskManager
 		public MainForm()
 		{
 			InitializeComponent();
+			this.Icon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location);
+			notifyIcon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location);
+			topmostToolStripMenuItem.Checked = Properties.Settings.Default.Topmost;
+			this.TopMost = topmostToolStripMenuItem.Checked;
+			hideWhenMinimizedToolStripMenuItem.Checked = Properties.Settings.Default.HideInTaskbar;
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void MainForm_Resize(object sender, EventArgs e)
+		{
+			if (hideWhenMinimizedToolStripMenuItem.Checked && this.WindowState == FormWindowState.Minimized)
+			{
+				this.ShowInTaskbar = false;
+			}
+			else this.ShowInTaskbar = true;
+		}
+
+		private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			this.WindowState = FormWindowState.Normal;
+			this.Show();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void topmostToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			topmostToolStripMenuItem.Checked = !topmostToolStripMenuItem.Checked;
+			this.TopMost = topmostToolStripMenuItem.Checked;
+			Properties.Settings.Default.Topmost = topmostToolStripMenuItem.Checked;
+			Properties.Settings.Default.Save();
+		}
+
+		private void hideWhenMinimizedToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			hideWhenMinimizedToolStripMenuItem.Checked = !hideWhenMinimizedToolStripMenuItem.Checked;
+			Properties.Settings.Default.HideInTaskbar = hideWhenMinimizedToolStripMenuItem.Checked;
+			Properties.Settings.Default.Save();
+		}
+
+		private void startNewProcessToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+		private void updateNowToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
